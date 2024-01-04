4f2b6161d74c3c0a7e2336de31592c79fa3cb8f3
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-05 00:06:06 +0200

    New package tailwind and styling files like custum and index.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section className="">Hero</section>
+      <section className="">Parallax</section>
+      <section className="">Services</section>
+      <section className="">Parallax</section>
+      <section className="">Portfolio1</section>
+      <section className="">Portfolio2</section>
+      <section className="">Portfolio3</section>
+      <section className="">Contact</section>
+    </div>
+  );
 };
 
 export default App;
