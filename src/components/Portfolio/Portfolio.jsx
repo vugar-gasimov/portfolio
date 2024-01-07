876425c888b3f5115c9c8e3f72927ffd2adecb24
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-07 02:35:06 +0200

    Few gramatical fications and Portfolio logic with some styles.

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import "./portfolio.scss";
+import { useRef } from "react";
 
 const items = [
   {
@@ -51,8 +52,24 @@ const Single = ({ item }) => {
 };
 
 const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
   return (
-    <div className="portfolio">
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
       {items.map((item) => (
         <Single key={item.id} item={item} />
       ))}
